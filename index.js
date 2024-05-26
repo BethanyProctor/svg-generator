9687f77c2c9f8b4452b9dfc423dd5bf603191be0
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-05-26 00:10:52 -0400

    bug fixes

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const inquirer = require('inquirer');
 const { Circle, Square, Triangle } = require('./lib/shapes');
 const questions = require('./lib/questions');
 
+//function to write the data from the answers to its own file
+const writeToFile = (fileName, data) => {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+
+//function to initialize the svg generation
 const init = () => {
     inquirer.prompt(questions)
-    .then
+    .then((responses) => {
+        console.log("Generating SVG...");
+        writeToFile("./logo.svg", questions({ ...responses }));
+        console.log('Generated logo.svg');
+    });
 }
 
 init();
