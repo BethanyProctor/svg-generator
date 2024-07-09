e2d37486537278bd3c5f171e3f725232c445f5fc
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-07-08 20:11:08 -0400

    fixed a bug

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { Shape, Circle, Square, Triangle } = require('./lib/shapes');
+const { Circle, Square, Triangle } = require('./lib/shapes');
 const questions = require('./lib/questions');
 
+// //create a SVG class that will generate the svg itself, the text, and shape
+// class Svg {
+//     constructor() {
+//     this.textEl = "";
+//     this.shapeEl = ""; 
+//     }
+//     render() {
+//         return `<svg height="300" width="200" xmlns="http://www.w3.org/2000/svg">${this.shapeEl}${this.textEl}</svg>`
+//     }
+//     styleText(textColor, text) {
+//         this.textEl = `<text x="100px" y="100px" font-size="60px" fill="${textColor}">${text}</text>`
+//     }
+//     setShape(shape) {
+//         this.shapeEl = shape.render()
+//     }
+// }
+
+//function to generate the logo
+const generateLogo = (data) => {
+    let shape = undefined;
+    if (data.shape === "Circle") {
+        shape = new Circle (data.fill, data.text, data.text_color)
+    } else if (data.shape ==="Triangle") {
+        shape = new Triangle (data.fill, data.text, data.text_color)
+    } else {
+        shape = new Square (data.fill, data.text, data.text_color)
+    }
+    return shape.render();
+}
+
 //function to write the data from the answers to its own file
-const writeToFile = (fileName, data) => {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+const  writeToFile = (fileName, data) => {
+    const content = generateLogo(data);
+    fs.writeFile(fileName, content, (error) => {
+        if (error) {
+            return console.log(error);
+        }
+     //log out a message when complete
+    console.log("SVG logo generated successfully!")
+    })
 }
 
 //function to initialize the svg generation
-const init = async () => {
-    let svgString = "";
-    let svgFile = "./logo.svg";
-
-    const responses = await inquirer.prompt(questions)
-    //set user shape
-    userShapeType = responses.shape;
-    //set user text
-    let userText = "";
-    //make sure the text entered is only 1-3 characters
-    if (responses.text.length > 0 && responses.text.length < 4) {
-        userText = responses.text
-    } else {
-        console.log("Please enter 1-3 characters.")
-        return;
-    }    
-    //set user text color
-    userFontColor = responses["text-color"];
-    //set user fill color
-    userFillColor = responses.fill;
-
-    //create new instance of shape chosen
-    let userShape;
-    if (userShapeType === "Circle") {
-        userShape = new Circle();
-    } else if (userShapeType === "Square") {
-        userShape = new Square();
-    } else {
-        userShape = new Triangle();
-    }
-
-    //create a new instance of the SVG with all elements
-    const svg = new Shape();
-    svg.styleText(userFontColor, userText);
-    svg.setShape(userShape);
-    svgString = svg.render();
-
-    //print the shape in a seperate file
-    console.log("Generating SVG...");
-    writeToFile(svgFile, svgString);
-    console.log('Generated logo.svg');
+const init = () => {
+    inquirer.prompt(questions)
+    .then (function (data) {
+        const fileName = "logo.svg";
+        writeToFile(fileName, data)
+    })
 }
 
 init();
